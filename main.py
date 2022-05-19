c54da1519002624d2d57eb58ee28824e2dbc2207
Kerwin <palewhx@gmail.com>
2022-05-20 00:37:31 +0800

    实现了获取前端'wx.login'的'code'并且发送到微信服务器获取返回的'session_key'和'openid'

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
+# coding=utf-8
+
 import json
 
+import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/userlogin', methods=['GET', 'POST'])
-def userPhoneLogin():
-    data = json.loads(request.get_data().decode('utf-8'))  # 将前端Json数据转为字典
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    appid = 'wx410bd3df4bf97389'
+    secret = '1d59463a0b6182cf5874a1319dba300d'
+    code = request.args.get('code')
+    print('code:', code)
+
+    request_params = {
+        'appid': appid,
+        'secret': secret,
+        'js_code': code,
+        'grant_type': 'authorization_code'
+    }
+
+    wx_login_api = 'https://api.weixin.qq.com/sns/jscode2session'
+    response_data = requests.get(wx_login_api, params=request_params)
+    res_data = response_data.json()
+    print(res_data)
+    return 'done'
 
 
 if __name__ == '__main__':
