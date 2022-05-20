cbf0bfe54e9678843c5aa3c7b64a7c19f8df95b3
Kerwin <palewhx@gmail.com>
2022-05-20 11:59:20 +0800

    增加解密 'WXBizDataCrypt.py'

---
+++ WXBizDataCrypt.py
@@ -0,0 +1,27 @@
+import base64
+import json
+from Crypto.Cipher import AES
+
+
+class WXBizDataCrypt:
+    def __int__(self, appid, session_key):
+        self.appid = appid
+        self.session_key = session_key
+
+    def decrypt(self, encrypted_data, iv):
+        # base64 decode
+        session_key = base64.b64decode(self.session_key)
+        encrypted_data = base64.b64decode(encrypted_data)
+        iv = base64.b64decode(iv)
+
+        cipher = AES.new(session_key, AES.MODE_CBC, iv)
+
+        decrypted = json.loads(self._unpad(cipher.decrypt(encrypted_data)))
+
+        if decrypted['watermark']['appid'] != self.appid:
+            raise Exception('Invalid Buffer')
+
+        return decrypted
+
+    def _unpad(self, s):
+        return s[:ord(s[len(s)-1:])]
